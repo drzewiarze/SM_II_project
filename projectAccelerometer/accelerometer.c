3a47856be2e08b05a0606a28ce67472333e967b6
j_ploskonka <165957970+drzewiarze@users.noreply.github.com>
2025-01-30 03:54:15 +0100

    Add files via upload

--- projectAccelerometer/accelerometer.c
+++ projectAccelerometer/accelerometer.c
@@ -21,51 +21,46 @@ uint8_t Accelerometer_ReadRegister(uint8_t reg);
 * Functions
 \******************************************************************************/
 /**
- * @brief Initialize the accelerometer.
+ * @brief  Configures the device, sets it to active mode, and enables interrupts.
  */
 void Accelerometer_Init(void) {
     uint8_t regValue;
 
-    regValue = Accelerometer_ReadRegister(MMA8451Q_REG_CTRL_REG1);
-    regValue &= ~0x01; // Clear ACTIVE bit to put accelerometer in standby mode
-    Accelerometer_WriteRegister(MMA8451Q_REG_CTRL_REG1, regValue);
-    regValue |= 0x01; // Set ACTIVE bit to start measurements
-    Accelerometer_WriteRegister(MMA8451Q_REG_CTRL_REG1, regValue);
+    regValue = Accelerometer_ReadRegister(MMA8451Q_REG_CTRL_REG1);          // Read control register 1
+    regValue &= ~0x01;                                                      // Clear ACTIVE bit to put accelerometer in standby mode
+    Accelerometer_WriteRegister(MMA8451Q_REG_CTRL_REG1, regValue);          // Write back to the register
+	
+    Accelerometer_WriteRegister(0x2D, 0x01);                                // Enable Data Ready (DRDY) interrupt
+    Accelerometer_WriteRegister(0x2E, 0x00);                                // Route DRDY interrupt to INT2 pin
+	
+    regValue |= 0x01;                                                       // Set ACTIVE bit to start measurements
+    Accelerometer_WriteRegister(MMA8451Q_REG_CTRL_REG1, regValue);          // Write back to the register
+}
+/**
+ * @brief Configure interrupt for the INT2 pin (PTA10).
+ *        Sets up the PTA10 pin as an input with an interrupt on the rising edge.
+ */
+void Interrupt_INT2_Pin_PTA10(void) {
+    SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;                                     // Enable clock for PORTA
+    PORTA->PCR[10] = PORT_PCR_MUX(1) | PORT_PCR_IRQC(0x9) ;                 // Configure PTA10 for rising-edge interrupt
+	PTA->PDDR &= ~(1 << 10);                                                // Set PTA10 as input
+    NVIC_EnableIRQ(PORTA_IRQn);                                             // Enable the interrupt in NVIC
 }
 /**
  * @brief Read accelerometer data.
- * @param data Pointer to structure to store the data.
+ * @param data Pointer to a structure to store the accelerometer data (X, Y, Z axes).
  */
-void Accelerometer_ReadData(struct accelerometer_data_t *ACC_DATA) {
+void Accelerometer_ReadData(struct accelerometer_data_t *data_XYZ) {
     uint8_t arrayXYZ[6];
-    
-    I2C_ReadRegBlock(MMA8451Q_I2C_ADDRESS, MMA8451Q_REG_OUT_X_MSB, 6, arrayXYZ);
+    uint8_t status = Accelerometer_ReadRegister(MMA8451Q_REG_STATUS);       // Read status register
+	
+    if (status) {	
+        I2C_ReadRegBlock(MMA8451Q_I2C_ADDRESS, MMA8451Q_REG_OUT_X_MSB, 6, arrayXYZ);
 
-    ACC_DATA->x = ((double)((int16_t)((arrayXYZ[0]<<8)|arrayXYZ[1])>>2)/(4096>> MMA8451Q_RANGE_2g)); // Convert 14-bit X data
-    ACC_DATA->y = ((double)((int16_t)((arrayXYZ[2]<<8)|arrayXYZ[3])>>2)/(4096>> MMA8451Q_RANGE_2g)); // Convert 14-bit Y data
-    ACC_DATA->z = ((double)((int16_t)((arrayXYZ[4]<<8)|arrayXYZ[5])>>2)/(4096>> MMA8451Q_RANGE_2g)); // Convert 14-bit Z data
-}
-/**
- * @brief Configure interrupt for INT2 pin.
- * @param enable Enable or disable interrupt.
- */
-void Accelerometer_ConfigureINT2(uint8_t enable) {
-    uint8_t regValue;
-
-    regValue = Accelerometer_ReadRegister(MMA8451Q_REG_CTRL_REG1);
-    if (enable) {
-        regValue |= (1 << 2); // Enable interrupt on INT2
-    } else {
-        regValue &= ~(1 << 2); // Disable interrupt
+        data_XYZ->x = ((double)((int16_t)((arrayXYZ[0]<<8)|arrayXYZ[1])>>2)/(4096>> MMA8451Q_RANGE_2g)); // Convert 14-bit X data
+        data_XYZ->y = ((double)((int16_t)((arrayXYZ[2]<<8)|arrayXYZ[3])>>2)/(4096>> MMA8451Q_RANGE_2g)); // Convert 14-bit Y data
+        data_XYZ->z = ((double)((int16_t)((arrayXYZ[4]<<8)|arrayXYZ[5])>>2)/(4096>> MMA8451Q_RANGE_2g)); // Convert 14-bit Z data
     }
-    Accelerometer_WriteRegister(MMA8451Q_REG_CTRL_REG1, regValue);
-}
-/**
- * @brief Check interrupt source.
- * @return Interrupt source register value.
- */
-uint8_t Accelerometer_GetInterruptSource(void) {
-    return Accelerometer_ReadRegister(MMA8451Q_REG_INT_SOURCE);
 }
 /******************************************************************************\
 * Private functions
@@ -76,15 +71,15 @@ uint8_t Accelerometer_GetInterruptSource(void) {
  * @param value Value to write.
  */
 void Accelerometer_WriteRegister(uint8_t reg, uint8_t value) {
-    I2C_WriteReg(MMA8451Q_I2C_ADDRESS, reg, value);
+    I2C_WriteReg(MMA8451Q_I2C_ADDRESS, reg, value);                         // Write value to the register over I2C
 }
 /**
- * @brief Read value from a specific register of the accelerometer.
+ * @brief Read a value from a specific register of the accelerometer.
  * @param reg Register address.
- * @return Value read from the register.
+ * @return The value read from the register.
  */
 uint8_t Accelerometer_ReadRegister(uint8_t reg) {
     uint8_t value;
-    I2C_ReadReg(MMA8451Q_I2C_ADDRESS, reg, &value);
-    return value;
+    I2C_ReadReg(MMA8451Q_I2C_ADDRESS, reg, &value);                         // Read value from the register over I2C
+    return value;                                                           // Return the read value
 }
--- projectAccelerometer/main.c
+++ projectAccelerometer/main.c
@@ -8,37 +8,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct accelerometer_data_t acc_data;
+volatile uint8_t odczytaj_dane_flaga = 0;                   // Flag in interrupt handling
+struct accelerometer_data_t acc_data;                       // Structure to store accelerometer data defined in accelerometer.h
+
+void PORTA_IRQHandler(void) {
+    if (PORTA->ISFR & (1 << 10)) {                          // Check if the interrupt was triggered by pin PTA10
+        PORTA->ISFR |= (1 << 10);                           // Clear the interrupt flag for pin PTA10 by writing '1' to the corresponding bit
+        odczytaj_dane_flaga = 1;                            // Set the flag to indicate that new data is available
+    }
+}
 
 int main(void) {
     char display[17] = {0x20};                              // Initialize the array to 16 characters + '\0'
-    enum Axis current_axis = AXIS_X;                        // Default axis
+    enum Axis current_axis = AXIS_X;                        // Default axis is X
 
-                                                            
-    LCD1602_Init();                                         // Initializations LCD
+    Interrupt_INT2_Pin_PTA10();                                                       
     Accelerometer_Init();                                   // Initializations MMA8451Q
+    LCD1602_Init();                                         // Initializations LCD
     TSI_Init();                                             // Initializations Touch Panel
 
     while (1) {
-    Accelerometer_ReadData(&acc_data);                      // Reading data from the accelerometer
-
-    handle_touch_input(&current_axis);                      // Axis change handling
-
+			
+		if (odczytaj_dane_flaga ) {                         // Check if new data is available
+        odczytaj_dane_flaga = 0;                            // Reset the data ready flag
+        Accelerometer_ReadData(&acc_data);                  // Read data from the accelerometer
+        }
+   
+    handle_touch_input(&current_axis);                      // Handle touch input to change the current axis
+	Accelerometer_ReadData(&acc_data);                      // Reading data from the accelerometer again
     switch (current_axis) {                                 // Displaying data for the selected axis
         case AXIS_X:
-            sprintf(display, "Axis X: %+4.2f g", acc_data.x);
-            update_lcd_with_progress_bar(display, acc_data.x);
+            sprintf(display, "Axis X: %+4.2f g", acc_data.x);   // Format and display data for axis X
+            update_lcd_with_progress_bar(display, acc_data.x);  // Update LCD with axis X data and progress bar
             break;
         case AXIS_Y:
-            sprintf(display, "Axis Y: %+4.2f g", acc_data.y);
-            update_lcd_with_progress_bar(display, acc_data.y);
+            sprintf(display, "Axis Y: %+4.2f g", acc_data.y);   // Format and display data for axis Y
+            update_lcd_with_progress_bar(display, acc_data.y);  // Update LCD with axis Y data and progress bar
             break;
         case AXIS_Z:
-            sprintf(display, "Axis Z: %+4.2f g", acc_data.z);
-            update_lcd_with_progress_bar(display, acc_data.z);
+            sprintf(display, "Axis Z: %+4.2f g", acc_data.z);   // Format and display data for axis Z
+            update_lcd_with_progress_bar(display, acc_data.z);  // Update LCD with axis Z data and progress bar
             break;
     }
 
-    DELAY(1000);                                            // better for the eye
+    //DELAY(1000);                                            // better for the eye
 }
 }
